c5aa1fd7d93e4252257055124275a9e98a778e1b
Gurita Claudiu <48445905+DBK200@users.noreply.github.com>
2023-07-04 17:50:00 +0000

    Update MyClass.java

--- src/main/java/MyClass.java
+++ src/main/java/MyClass.java
@@ -2,5 +2,7 @@ public class MyClass {
     public static void main(String[] args) {
         System.out.println("wefwef");
         System.out.println("wefwef");
+        System.out.println("wefwef");
+        
     }
 }
